f4d0e8e0aae48c031ff9b631a5d92ac12f8f1258
ayalajair <ayalajair@gmail.com>
2022-08-23 21:38:57 -0300

    Se implementó bootstrap para el navegador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header.js';
 import Nav from './components/Nav/Nav.js';
 
 class App extends React.Component {
   render() {
     return(
       <div>
-        <Header/>
         <Nav/>
       </div>
     )
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,22 +1,36 @@
 import React from "react";
 import './Nav.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import img from '../../assets/logo-delfitness.png'
 
 const Nav = () => {
         return (
-            <nav className="navbar bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/" >
-                            <img src="#" alt="logo" width="30" height="24" className="d-inline-block align-text-top"/>
-                            Delfitness
-                        </a>
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            </ul>
-                    </div>
+        <header className="navbar navbar-dark bg-dark navbar-expand-lg bd-navbar sticky-top">
+                <nav className="container-xxl bd-gutter flex-wrap flex-lg-nowrap" >
+                                <a className="navbar-brand" href="/" >
+                                        <img src={img} alt="logo" width="150" height="40" className="d-inline-block align-text-top"/>
+                                </a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                        <li className="nav-item">
+                                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                        </li>
+                                        <li className="nav-item">
+                                                <a className="nav-link"  href="/">Indumentaria</a>
+                                        </li>
+                                        <li className="nav-item">
+                                                <a className="nav-link"  href="/">Accesorios</a>
+                                        </li>
+                                        <li className="nav-item">
+                                                <a className="nav-link"  href="/">Suplementos</a>
+                                        </li>
+                                </ul>
+                                </div>
             </nav>
+        </header>
         )
     }
 
