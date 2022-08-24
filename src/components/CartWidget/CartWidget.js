efec828d8aaccfee240bfc99a73ae4cc1ca779a8
ayalajair <ayalajair@gmail.com>
2022-08-24 17:19:43 -0300

    Se crea el componente CartWidget

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './CartWidget.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CartWidget = ()=> {
+    return ( 
+            <i className="bi bi-cart" style={{color:"#fff"} }> </i>
+    )
+}
+
+export default CartWidget;
+
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Nav.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import img from '../../assets/logo-delfitness.png'
+import CartWidget from "../CartWidget/CartWidget";
 
 const Nav = () => {
         return (
@@ -13,7 +14,7 @@ const Nav = () => {
                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                         <span class="navbar-toggler-icon"></span>
                                 </button>
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                         <li className="nav-item">
                                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
@@ -27,11 +28,16 @@ const Nav = () => {
                                         <li className="nav-item">
                                                 <a className="nav-link"  href="/">Suplementos</a>
                                         </li>
+                                        <li></li>
                                 </ul>
                                 </div>
-            </nav>
+                                <div className="text-end">
+                                        <CartWidget/>
+                                </div>
+                                
+                </nav>
         </header>
         )
-    }
+}
 
 export default Nav;
