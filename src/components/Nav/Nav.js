0a1c316012dad0112846e0d82ab8c6f8b5a8e314
ayalajair <ayalajair@gmail.com>
2022-08-25 19:02:15 -0300

    Se modifcó el CartWidget

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -11,8 +11,8 @@ const Nav = () => {
                                 <a className="navbar-brand" href="/" >
                                         <img src={img} alt="logo" width="150" height="40" className="d-inline-block align-text-top"/>
                                 </a>
-                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span class="navbar-toggler-icon"></span>
+                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span className="navbar-toggler-icon"></span>
                                 </button>
                                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -30,9 +30,9 @@ const Nav = () => {
                                         </li>
                                         <li></li>
                                 </ul>
-                                </div>
                                 <div className="text-end">
                                         <CartWidget/>
+                                </div>
                                 </div>
                                 
                 </nav>
