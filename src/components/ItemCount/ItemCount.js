fb1c5b578d4c731085b0ec8ab347669454f696cd
ayalajair <ayalajair@gmail.com>
2022-08-29 21:34:28 -0300

    Se crearon funciones p incrementar y decrementar

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -3,12 +3,24 @@ import './ItemCount.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ItemCount = ()=> {
+    const [counter, setCounter] = useState (0);
+
+    const handlerCounterDown = () => {
+        if (counter>0) {
+            setCounter(counter - 1);
+        }
+    };
+    const handlerCounterUp = () => {
+        if (counter<10) {
+            setCounter(counter + 1);
+        }
+    };
 
     return (
         <div className="itemCounter">
-            <button className="btn btn-outline-dark">-</button>
-            <span>0</span>
-            <button className="btn btn-outline-dark">+</button>
+            <button className="btn btn-outline-dark" onClick={handlerCounterDown}>-</button>
+            <span>{counter}</span>
+            <button className="btn btn-outline-dark" onClick={handlerCounterUp}>+</button>
             <div>
                 <button className="btn btn-dark">Agregar al carrito</button>
             </div>
