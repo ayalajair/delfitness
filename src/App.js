22bb0bf4da7dbd1f41c805a292a4879098a5e8fc
ayalajair <ayalajair@gmail.com>
2022-09-02 15:27:13 -0300

    Se solucionaron errores de rutas

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 import Nav from './components/Nav/Nav.js';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 class App extends React.Component {
   render() {
     return(
-      <Router>
         <div className='App'>
           <Nav/>
-          <Routes>
-            <Route path='/' element={<ItemListContainer/>}/>
-            <Route path='/category' element={<ItemListContainer/>}/>
-            <Route path='/detail' element={<ItemDetailContainer/>}/>
-          </Routes>
+          <ItemListContainer/>
+          <ItemDetailContainer/>
         </div>
-      </Router>
     )
   }
 }
