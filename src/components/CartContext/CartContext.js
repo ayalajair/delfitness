fa21878298651b9b3b99bdacbcb5b61649b0f84f
ayalajair <ayalajair@gmail.com>
2022-09-13 22:41:54 -0300

    se modificaron estilos

--- src/components/CartContext/CartContext.js
+++ src/components/CartContext/CartContext.js
@@ -1,8 +1,6 @@
 import React, { createContext, useState }   from "react";
 export const CartContext = createContext ([]);
 
-
-
 const CartProvider = ({children}) => {
 
     const [cart, setCart] = useState ([]);
