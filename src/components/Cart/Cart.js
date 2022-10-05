83d78f8bf6f6025b2ac996d6b4e0ec3ff5669ac2
ayalajair <ayalajair@gmail.com>
2022-10-04 21:31:31 -0300

    Se creó la finalización de compra Hardcodeado

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import './Cart.css'
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../CartContext/CartContext";
 import ItemCart from "../ItemCart/ItemCart";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {db} from '../../Firebase/firebaseConfig'
+import { addDoc, collection } from "firebase/firestore";
 
 const Cart = () => {
     const{cart, totalCart} = useContext(CartContext);
+    const [idOrder, setIdOrder] = useState("");
+
+
+    const handleClick = () => {
+        const orderCollection = collection (db, 'orders');
+        addDoc (orderCollection, {
+            buyer: {
+                name: 'Jair',
+                mail: 'ayalajair@gmail.com',
+                phone: '12356548',
+                adress: 'calle falsa 1234'
+            },
+            items: cart.map (product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+            total: totalCart()
+        }).then ((res) => setIdOrder (res.id))
+        
+
+    }
 
     if (cart.length === 0) {
         return (
@@ -21,6 +41,7 @@ const Cart = () => {
         <div>
             {cart.map (product => <ItemCart key={product.id} product={product}/>)} 
             <p className="fs-3 fw-bold ">Total: ${totalCart()}</p>
+            <button onClick={handleClick}>Finalizar compra</button>
         </div>
     )
 }
